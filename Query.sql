e59a2637f00d19c8f763928076ca52302bfb077d
localhost3306-quan <58687197+localhost3306-quan@users.noreply.github.com>
2021-06-23 12:23:30 +0700

    first commit

---
+++ Query.sql
@@ -0,0 +1,51 @@
+drop table CartItem
+drop table Cart
+
+use master
+go
+drop database UNIFY
+use master
+go
+create database UNIFY
+go
+use UNIFY
+go
+
+
+create table [User](
+	id int identity,
+	email varchar(50),
+	username varchar(50) unique not null,
+	[password] varchar(36) not null,
+	avatar varchar(50),
+	role_id int,
+	primary key(id)
+)
+create table Category(
+	cate_id int identity primary key,
+	cate_name nvarchar(255) not null,
+)
+
+create table Product(
+	id int identity primary key,
+	[name] nvarchar(255) not null,
+	price float ,
+	cate_id int references Category(cate_id) not null,
+	des varchar(2000),
+	image varchar(50),
+)
+
+
+create table Cart(
+	id varchar(50) primary key,
+	u_id int references [User](id) not null ,
+	buyDate date
+)
+create table CartItem(
+	id varchar(50) primary key,
+	quantity int,
+	unitPrice float,
+	pro_id int references Product(id) not null,
+	cat_id varchar(50) references Cart(id) not null
+)
+
---
+++ README.md
@@ -0,0 +1,21 @@
+# UNIFY
+Website bán hàng  sử dụng java/jsp, servlet, jstl
+Demo: https://www.youtube.com/watch?v=_uWHezOxq0U&t
+
+Có tích hợp live chat, send email khi đăng kí , thanh toán đơn hàng thành công , ckediter để edit văn bản
+
+** Các chức năng chính
+- Cho phép người dùng truy cập vào website xem các sản phẩm
+
+- Đăng nhập, đăng kí tài khoản
+
+- Có phân quyền user và admin
+
+- Thêm sửa xóa sản phẩm, tài khoản người dùng
+
+- Đặt hàng - …
+
+-Quản lý đơn hàng, giỏ hàng
+
+
+Website bán hàng jsp/servlet theo mô hình MVC dễ hiểu và dễ phát triển. Thích hợp cho các bạn làm bài tập lớn, phát triển web
